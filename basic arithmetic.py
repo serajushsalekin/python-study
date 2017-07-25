ad10ffe8bdb0cc78fcbf87eba43b5a5370b26fac
Salekin <serajushsalekin@gmail.com>
2017-07-25 23:30:06 +0600

    dictionary
    complicated #1

--- basic arithmetic.py
+++ basic arithmetic.py
@@ -1,3 +1,3 @@
 #dictionary
-myDic_object = {'a':[1,2,3],'b':"hello"}
-print(myDic_object['a'])
+myDic_object = {'a':[1,2,3],'b':"hello",'c':{'group':1200,'non_group':850}}
+print(myDic_object['c']['group'])
